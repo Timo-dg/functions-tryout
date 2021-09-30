5961a13c2c586d980f71e10c4657ef29fd746559
Timo de Gruiter <timo@EDGITNEW.local>
2021-09-30 12:15:44 +0200

    opdracht afgerond

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+
+def getallenreeks():
+    a,b = 0,1
+    for x in range(20):
+        print('fibonacci getal ' + str(x+1) + ': ' + str(a))
+        a,b = b,a+b
+        print('ratio: ' + str(a/b))
+getallenreeks()
